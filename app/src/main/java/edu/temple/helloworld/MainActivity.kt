3121de73b0fb23e718bab66ebe2c10f13be61025
Jun Zhou <tun40202@temple.edu>
2023-09-14 12:00:31 -0400

    declear properties and add event listener

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -6,10 +6,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+
+
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var myButton:Button
+    lateinit var text:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,11 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        myButton = findViewById(R.id.clickMeButton)
+        text=findViewById(R.id.nameEditText)
+        myButton.setOnClickListener{
+            displayTextView.text="Hello, ${text.text}"
+        }
 
         
         // Respond to button click event per specifications
